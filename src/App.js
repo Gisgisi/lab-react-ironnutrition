d8eac4f5468aa009d4a25cf8ebfe69a4c1917a8c
John Doe <atarod.shirin@gmail.com>
2022-11-16 16:02:59 +0100

    added FoodBox component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
+import FoodBox from './Components/FoodBox.js';
 
 function App() {
   const [food, setfoods] = useState(foods)
   return (
     <div className="App">
-        {food.map((thefood) => 
-          <div>         
-            <p>{thefood.name}</p>
-            <img src={thefood.image}/>
-          </div>
-        )}
+      {food.map((thefood) =>
+        <div>
+          <p>{thefood.name}</p>
+          <img src={thefood.image} width="10%" />
+        </div>
+      )}
+      <FoodBox food={{
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
     </div>
+
   );
 }
 
